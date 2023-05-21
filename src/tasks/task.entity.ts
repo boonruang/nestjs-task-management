8f489ff7e723c6f0296e5b9d1e793158bf2e5a9e
boonruang <scubatoy@gmail.com>
2023-05-21 21:45:55 +0700

    73. Serialize User Data

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,11 +1,13 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 import { User } from 'src/auth/user.entity';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class Task {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
   @Column()
   title: string;
 
@@ -16,5 +18,6 @@ export class Task {
   status: TaskStatus;
 
   @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  @Exclude({ toPlainOnly: true })
   user: User;
 }
