080cc66d2d6247ec16bbd252509306e23f5cb56f
boonruang <scubatoy@gmail.com>
2023-05-21 13:30:38 +0700

    60. Error Handling: Username Conflicts

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -6,11 +6,19 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 
+import * as bcrypt from 'bcrypt';
+
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
+
+    // hash
+    const asd = bcrypt(password);
+
+    console.log(asd);
+
     try {
       await this.save(user);
     } catch (error) {
